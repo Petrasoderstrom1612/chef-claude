c5c6844b7c7f39abb73901dd4d6375abaf323cd4
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-04-11 13:37:25 +0200

    Initial commit

---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import chefClaudeLogo from "./assets/chef-claude-icon.png"
+
+export const Header = () => {
+  return (
+    <header>
+            <img src={chefClaudeLogo}/>
+            <h1>Chef Claude</h1>
+        </header>
+  )
+}
