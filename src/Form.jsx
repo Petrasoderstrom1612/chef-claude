1cc72dbbcd8ef55ef3813ca659dd083fd12b8bb7
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-04-14 13:11:09 +0200

    added an array that I loop over and started a function just to listen on the form submit

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
 
 export const Form = () => {
+  const ingrediences = ["egg", "tomato", "basil"]
+
+  const ingrediencesList = ingrediences.map((oneIngredience) => {
+    return <li key={oneIngredience} >{oneIngredience}</li>
+  })
+
+  const addIngredience = (e) => {
+    e.preventDefault()
+    console.log("clicked")
+  }
+
   return (
     <main>
-      <form className='add-ingredient-form'>
-        <input type="text" placeholder="e.g. oregano" aria-label="Add ingredient"/>
+      <form className='add-ingredient-form' onSubmit={addIngredience}>
+        <input 
+        type="text" 
+        placeholder="e.g. oregano" 
+        aria-label="Add ingredient"
+        name="ingredient"
+        />
         <button>Add ingredient</button>
       </form>
+      <ul>{ingrediencesList}</ul>
     </main>
   )
 }
