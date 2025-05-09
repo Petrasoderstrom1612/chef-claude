c585d6763022d4f0eeb82d5cf730b6a62e3db00e
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-05-09 09:52:10 +0200

    moved components into a component folder

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import chefClaudeLogo from "./assets/chef-claude-icon.png"
+import chefClaudeLogo from "../assets/chef-claude-icon.png"
 
 export const Header = () => {
   return (
